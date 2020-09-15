bcae40f87f53c484ad9229fb2c125ad9e1e20ced
Julia Paz <juliapazpinheiro@gmail.com>
2020-09-14 21:03:18 -0300

    Adicionar arquivos de projeto.

---
+++ SocialNetwork/Data/SocialNetworkContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SocialNetwork.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SocialNetwork.Data
+{
+    public class SocialNetworkContext : DbContext
+    {
+        public DbSet<Post> Posts { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseSqlServer("Server=tcp:network-db-server.database.windows.net,1433;Initial Catalog=Network-db;Persist Security Info=False;User ID=juliapazpinheiro;Password=Capadocia2012!;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
+        }
+    }
+}
